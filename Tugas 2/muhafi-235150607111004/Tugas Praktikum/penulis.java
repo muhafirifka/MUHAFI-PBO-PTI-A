82c7ce38a8c882a80bcf18774c2171d6d406690a
muhafirifka <160575488+muhafirifka@users.noreply.github.com>
2024-03-13 10:09:23 +0700

    Create penulis.java

---
+++ Tugas 2/muhafi-235150607111004/Tugas Praktikum/penulis.java
@@ -0,0 +1,11 @@
+public class penulis {
+    private String nama;
+
+    public penulis(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+}
